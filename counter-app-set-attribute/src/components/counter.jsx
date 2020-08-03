c1c17a8f96c2545ce5d9d61d65eabb2b562fc162
Shivam Sinha <shivam.si@media.net>
2020-08-03 22:06:42 +0530

    component setAttribbutes

---
+++ counter-app-set-attribute/src/components/counter.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/400",
+  };
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
+  render() {
+    return (
+      <div>
+        <img src={this.state.imageUrl} alt="" />
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increments</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
