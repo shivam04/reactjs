d2ad22568c2e5e4566ef41cb50a5ad2653102e12
Shivam Sinha <shivam.si@media.net>
2020-08-05 00:26:10 +0530

    lifecycle hook

---
+++ counter-app-lifecycle-hook/src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  constructor() {
+    super();
+    console.log("App - Constructor");
+    //this.state = this.props.something
+    //this.setState gives error
+  }
+
+  componentDidMount() {
+    //Ajax call
+    //this.setState({]})
+    console.log("App - Mounted");
+  }
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    console.log("App - Rendered");
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counters={this.state.counters}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
