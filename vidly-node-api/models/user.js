d3725115eabc9b7263a54b5e178640ea71e44bcf
Shivam Sinha <shivam.si@media.net>
2020-08-07 15:45:37 +0530

    node api

--- vidly-node-api/models/user.js
+++ vidly-node-api/models/user.js
@@ -1,13 +1,13 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 5,
+    minlength: 2,
     maxlength: 50
   },
   email: {
@@ -26,22 +26,40 @@ const userSchema = new mongoose.Schema({
   isAdmin: Boolean
 });
 
-userSchema.methods.generateAuthToken = function() { 
-  const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      isAdmin: this.isAdmin
+    },
+    config.get("jwtPrivateKey")
+  );
   return token;
-}
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
   const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required()
+    name: Joi.string()
+      .min(2)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
   };
 
   return Joi.validate(user, schema);
 }
 
-exports.User = User; 
+exports.User = User;
 exports.validate = validateUser;
--- vidly-node-api/routes/rentals.js
+++ vidly-node-api/routes/rentals.js
@@ -1,34 +1,38 @@
-const {Rental, validate} = require('../models/rental'); 
-const {Movie} = require('../models/movie'); 
-const {Customer} = require('../models/customer'); 
-const mongoose = require('mongoose');
-const Fawn = require('fawn');
-const express = require('express');
+const { Rental, validate } = require("../models/rental");
+const { Movie } = require("../models/movie");
+const { Customer } = require("../models/customer");
+const auth = require("../middleware/auth");
+const mongoose = require("mongoose");
+const Fawn = require("fawn");
+const express = require("express");
 const router = express.Router();
 
 Fawn.init(mongoose);
 
-router.get('/', async (req, res) => {
-  const rentals = await Rental.find().sort('-dateOut');
+router.get("/", auth, async (req, res) => {
+  const rentals = await Rental.find()
+    .select("-__v")
+    .sort("-dateOut");
   res.send(rentals);
 });
 
-router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const customer = await Customer.findById(req.body.customerId);
-  if (!customer) return res.status(400).send('Invalid customer.');
+  if (!customer) return res.status(400).send("Invalid customer.");
 
   const movie = await Movie.findById(req.body.movieId);
-  if (!movie) return res.status(400).send('Invalid movie.');
+  if (!movie) return res.status(400).send("Invalid movie.");
 
-  if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie not in stock.");
 
-  let rental = new Rental({ 
+  let rental = new Rental({
     customer: {
       _id: customer._id,
-      name: customer.name, 
+      name: customer.name,
       phone: customer.phone
     },
     movie: {
@@ -40,25 +44,29 @@ router.post('/', async (req, res) => {
 
   try {
     new Fawn.Task()
-      .save('rentals', rental)
-      .update('movies', { _id: movie._id }, { 
-        $inc: { numberInStock: -1 }
-      })
+      .save("rentals", rental)
+      .update(
+        "movies",
+        { _id: movie._id },
+        {
+          $inc: { numberInStock: -1 }
+        }
+      )
       .run();
-  
+
     res.send(rental);
-  }
-  catch(ex) {
-    res.status(500).send('Something failed.');
+  } catch (ex) {
+    res.status(500).send("Something failed.");
   }
 });
 
-router.get('/:id', async (req, res) => {
-  const rental = await Rental.findById(req.params.id);
+router.get("/:id", [auth], async (req, res) => {
+  const rental = await Rental.findById(req.params.id).select("-__v");
 
-  if (!rental) return res.status(404).send('The rental with the given ID was not found.');
+  if (!rental)
+    return res.status(404).send("The rental with the given ID was not found.");
 
   res.send(rental);
 });
 
-module.exports = router; 
+module.exports = router;
