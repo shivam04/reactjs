e9824d6e9cf4ad9b9ee0048ba1c2fc9626ea7420
Shivam Sinha <shivam.si@media.net>
2020-08-11 14:18:06 +0530

    context-using-contextType

---
+++ react-context-hooks/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import BookList from "./components/BookList";
+import NavBar from "./components/NavBar.jsx";
+import ThemeContextProvider from "./contexts/ThemeContext";
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <NavBar />
+        <BookList />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
